93cf4e7ed2752340fbffa887ad2724d634a86563
Olga Seraya <serayaov@gmail.com>
2020-12-19 16:09:28 +0300

    fix

--- src/test/java/spring/deserializers/CurrencyXmlDeserializerTest.java
+++ src/test/java/spring/deserializers/CurrencyXmlDeserializerTest.java
@@ -26,7 +26,7 @@ public class CurrencyXmlDeserializerTest {
                     "<Valute ID=\"R01235\"><NumCode>840</NumCode><CharCode>USD</CharCode><Nominal>1</Nominal><Name>Доллар США</Name><Value>998.12</Value></Valute>" +
                     "<Valute ID=\"R01239\"><NumCode>978</NumCode><CharCode>EUR</CharCode><Nominal>1</Nominal><Name>Евро</Name><Value>997.12</Value></Valute>" +
                     "<Valute ID=\"R01350\"><NumCode>124</NumCode><CharCode>CAD</CharCode><Nominal>1</Nominal><Name>Канадский доллар</Name><Value>996.12</Value></Valute>" +
-                    "<Valute ID=\"R01625\"><NumCode>702</NumCode><CharCode>SGD</CharCode><Nominal>1</Nominal><Name>Сингапурский доллар</Name><Value>995.12</Value></Valute>";
+                    "<Valute ID=\"R01625\"><NumCode>702</NumCode><CharCode>SGD</CharCode><Nominal>1</Nominal><Name>Сингапурский доллар</Name><Value>995.12</Value></Valute></ValCurs>";
             assertEquals(new Currency("TestDate", 999.12, 997.12, 996.12, 995.12, 1000.12, 998.12), deserializer.deserialize(testXml));
         } catch (Exception e) {
             fail();
